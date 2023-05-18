572c2128cabcfde07b8f0c3705b31b8f28ebd23d
wayne00182 <125425830+wayne00182@users.noreply.github.com>
2023-05-18 15:09:54 +0800

    Add files via upload

---
+++ week14-2_timer_play/main.cpp
@@ -0,0 +1,37 @@
+#include <GL/glut.h>
+float angle= 0;
+void timer(int t)
+{
+    glutTimerFunc(500, timer, t+1);
+    angle += 90;
+    glutPostRedisplay();
+}
+void keyboard(unsigned char key, int x, int y)
+{
+    glutTimerFunc(0, timer, 0);
+}
+
+
+void display()
+{
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+    glPushMatrix();
+     glRotatef(angle, 0, 0, 1);
+     glutSolidTeapot( 0.3 );
+   glPopMatrix();
+   glutSwapBuffers();
+}
+   int main(int argc, char**argv)
+{
+
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+  glutCreateWindow("week14");
+
+  glutKeyboardFunc(keyboard);
+  glutDisplayFunc(display);
+
+  glutMainLoop();
+
+}
